7bc042dcb4aae75e6f334e86cab1eb72376c9d93
Ram Singh <rams48705@gmail.com>
2021-06-24 19:28:29 +0530

    2nd commit

--- movie1.js
+++ movie1.js
@@ -3,15 +3,19 @@ async function getNow() {
     let res = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=e8b0a2f0`)
     var data = await res.json()
      collection(data)
-     console.log(data);
+     
     
 }
-// getNow()
+
 
 function collection(data) {
     let inp = document.getElementById('name').value;
     if (data.Title == inp) {
         let mainDiv = document.getElementById('cont')
+        
+        while (mainDiv.hasChildNodes()) {  
+        mainDiv.removeChild(mainDiv.firstChild);
+        }
         let img = document.createElement('img')
         img.src = data.Poster;
         let actors = document.createElement('p')
@@ -35,6 +39,18 @@ function collection(data) {
         mainDiv.append(img,actors,awards,Country,Language,Released, imdbRating)
 
     }
+    else {
+        
+        let mainDiv = document.getElementById('cont')
+        
+        while (mainDiv.hasChildNodes()) {  
+        mainDiv.removeChild(mainDiv.firstChild);
+        }
+        
+        let h2 = document.createElement('h2')
+        h2.innerHTML = `The Movie is not Available here`
+        mainDiv.append(h2)
+
+    }
 }
 
-// collection()
